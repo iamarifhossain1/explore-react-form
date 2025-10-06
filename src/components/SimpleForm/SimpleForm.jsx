78ccdc18ad0e52bbb79edb73a3d3988bfb01fff3
iamarifhossain1 <sporshoahmed2667@gmail.com>
2025-10-06 10:55:14 +0600

    simple-react-form

--- src/components/SimpleForm/SimpleForm.jsx
+++ src/components/SimpleForm/SimpleForm.jsx
@@ -2,15 +2,19 @@ import React from 'react';
 
 const SimpleForm = () => {
 
-    const handleSubmit = () => {
-    console.log('Form Submitted');
+    const handleSubmit = (e) => {
+        e.preventDefault();
+    console.log(e.target.name.value);
+    console.log(e.target.email.value);
     
   } 
 
     return (
         <div>
             <form onSubmit={handleSubmit}>
-                <input type="text" />
+                <input type="text" placeholder='Your Name' name='name'/>
+                <br />
+                <input type="email" name="email" id="" placeholder='Your Email'/>
                 <br/>
                 <input type="submit" value="Submit" />
             </form>
