1fbdae71e64a10e704cae689cba54b9014c60ceb
iamarifhossain1 <sporshoahmed2667@gmail.com>
2025-10-07 14:45:09 +0600

    context-api

--- src/components/FamilyTree/FamilyTree.jsx
+++ src/components/FamilyTree/FamilyTree.jsx
@@ -1,21 +1,26 @@
-import React, { createContext } from 'react';
+import React, { createContext, useState } from 'react';
 import GrandPa from './GrandPa';
 import './FamilyTree.css'
 
 export const AssetContext = createContext('')
+export const MoneyContext  = createContext(0);
 
 
 const FamilyTree = () => {
 
+    const [money, setMoney] = useState(0)
     const asset = 'diamond'
     const newAsset = 'gold'
 
     return (
         <div className='family-tree'>
             <h2>Family Tree</h2>
-            <AssetContext.Provider value={newAsset}>
-                <GrandPa asset={asset}></GrandPa>
-            </AssetContext.Provider>
+            <h2>Total Family Money: {money}</h2>
+            <MoneyContext value={[money, setMoney]}>
+                <AssetContext.Provider value={newAsset}>
+                    <GrandPa asset={asset}></GrandPa>
+                </AssetContext.Provider>
+            </MoneyContext>
         </div>
     );
 };
